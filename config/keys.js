b04d19a8dc48602e23631f3f4979ea15047c52c4
Shubham Shukla <shk.shubham@gmail.com>
2017-11-18 13:14:03 +0000

    keys added

---
+++ config/keys.js
@@ -0,0 +1,31 @@
+const mongodb = {
+  url: "mongodb://root:sa123@ds157185.mlab.com:57185/node-blog"
+}
+
+const google = {
+  clientID: "828100595824-3hlrn08u1uhd0dngsbe1l08ub28ecln2.apps.googleusercontent.com",
+  clientSecret: "tCDwN2ht9Qw8YuCLy4EwJJmf"
+};
+
+const stack = {
+  clientID: "11276",
+  clientSecret: "3OcdczzGWALcuyNglP4Yrg(("
+};
+
+const github = {
+  clientID: "d84b3496f57c0057952e",
+  clientSecret: "ef4219083b62633b0f33634c50aa788823365f34"
+};
+
+
+const session = {
+  cookieKey: "keytoencryptthecookie"
+}
+
+module.exports = {
+  google,
+  mongodb,
+  session,
+  github,
+  stack
+};
